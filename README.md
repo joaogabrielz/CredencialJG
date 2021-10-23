75158515d024ce3731baabd527cc40cc116d9424
joaogabrielz <zacarias1021@gmail.com>
2021-10-23 19:51:33 -0400

    adicionado novos elementos e animacoes alem de javascript

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CredencialJG
 projeto credencial do evento nlw heat da RocketSeat
+
+# botoes Youtube e Twitter
+Youtube e Twitter inativos pois nao utilizo os mesmos
---
+++ script.js
@@ -0,0 +1,13 @@
+function textTypeW(element){
+  
+    const textArray = element.innerHTML.split(''); // > separa cada letra dos elementos
+   // > console.log(textArray)  > teste no console
+   element.innerHTML = ""; // > zera o elemento <p>
+    textArray.forEach((letra, i) => {
+       setTimeout(() => element.innerHTML += letra , 75 * i )
+       // > cria animacao passando a funcao que recebe as letras e faz 75ms vezes o index, em nova sintaxe
+    }) 
+    // > para cada = 'forEach' , letra e index
+}
+const texto = document.querySelector('p') // > atribui a variavel texto o elemento <p>
+textTypeW(texto); // > chama a função
